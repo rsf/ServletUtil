a622665518a00cc87edef953b876192a99e66359
Antranig Basman <amb26@ponder.org.uk>
2006-08-10 22:03:59 +0000

    Noted that Select fixup is now mistimed, unfixable Blank values for input components will always be blank Creation fail guard for RSACBL to avoid misleading messages

--- src/uk/org/ponder/rsac/RSACBeanLocator.java
+++ src/uk/org/ponder/rsac/RSACBeanLocator.java
@@ -38,20 +38,22 @@ import uk.org.ponder.util.RunnableWrapper;
 import uk.org.ponder.util.UniversalRuntimeException;
 
 /**
- * The central class managing the Request Scope Application Context. 
- * <p>The principal method useful to users is <code>getBeanLocator()</code>
- * which returns a BeanLocator holding the request context for the current
- * thread. For each thread entering an RSAC request, it must call 
+ * The central class managing the Request Scope Application Context.
+ * <p>
+ * The principal method useful to users is <code>getBeanLocator()</code> which
+ * returns a BeanLocator holding the request context for the current thread. For
+ * each thread entering an RSAC request, it must call
  * <code>startRequest()</code> before acquiring any request beans, and
- * <code>endRequest()</code> at the end of its cycle (the latter is most important).
+ * <code>endRequest()</code> at the end of its cycle (the latter is most
+ * important).
  * <p>
  * In a "pure RSAC" application, the request logic will be defined by the
  * getting of a single "root bean" from the BeanLocator, although initial setup
  * and any proxies may require additional calls to <code>getBeanLocator()</code>.
- * <p>This class
- * will be due for some refactoring as soon as the next piece of functionality
- * gets added, getting on for 400 lines. Please note that this class currently
- * illegally casts BeanDefinitions received from Spring to
+ * <p>
+ * This class will be due for some refactoring as soon as the next piece of
+ * functionality gets added, getting on for 400 lines. Please note that this
+ * class currently illegally casts BeanDefinitions received from Spring to
  * AbstractBeanDefinition, which is a potential dependency weakness. This
  * approach is known to work with Spring 1.1.2 through 1.2.6.
  * 
@@ -61,9 +63,9 @@ import uk.org.ponder.util.UniversalRuntimeException;
 
 public class RSACBeanLocator implements ApplicationContextAware,
     BeanDefinitionSource {
-  /** The standard bean name for the RSACBeanLocator **/
+  /** The standard bean name for the RSACBeanLocator * */
   public static String RSAC_BEAN_LOCATOR_NAME = "RSACBeanLocator";
-  
+
   private static CreationMarker BEAN_IN_CREATION_OBJECT = new CreationMarker(0);
   private static String REQUEST_STARTED_KEY = ".request  started";
   private ConfigurableApplicationContext blankcontext;
@@ -93,10 +95,12 @@ public class RSACBeanLocator implements ApplicationContextAware,
   // return new PerRequestInfo(RSACBeanLocator.this, lazysources);
   // }
   // };
-  // We do not use initialValue here because of bug 
+  // We do not use initialValue here because of bug
   // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5025230 which corrupts
-  // the table when initialising a further ThreadLocal from within initialValue().
-  // in this case the ThreadLocal is created within CGLib while instantiating the
+  // the table when initialising a further ThreadLocal from within
+  // initialValue().
+  // in this case the ThreadLocal is created within CGLib while instantiating
+  // the
   // RSACLazyTargetSource proxies.
   private ThreadLocal threadlocal = new ThreadLocal();
 
@@ -108,7 +112,6 @@ public class RSACBeanLocator implements ApplicationContextAware,
     }
     return pri;
   }
-
 
   /**
    * Starts the request-scope container for the current thread.
@@ -124,10 +127,11 @@ public class RSACBeanLocator implements ApplicationContextAware,
     pri.beans.set(REQUEST_STARTED_KEY, BEAN_IN_CREATION_OBJECT);
   }
 
-  /** Determines whether the container has already been started for the
-   * current thread.
+  /**
+   * Determines whether the container has already been started for the current
+   * thread.
    */
-  
+
   public boolean isStarted() {
     PerRequestInfo pri = getPerRequest();
     return pri.beans.locateBean(REQUEST_STARTED_KEY) != null;
@@ -223,8 +227,7 @@ public class RSACBeanLocator implements ApplicationContextAware,
    * harder to resolve bean classes than Spring generally does, through walking
    * chains of factory-methods.
    * 
-   * @param clazz
-   *          A class or interface class to be searched for.
+   * @param clazz A class or interface class to be searched for.
    * @return A list of derived bean names.
    */
   public String[] beanNamesForClass(Class clazz) {
@@ -312,12 +315,12 @@ public class RSACBeanLocator implements ApplicationContextAware,
     // contexts.
     // NB - we check the container since some fiend might have thrown it in
     // manually on inchuck - but actually this is faster than Spring anyway.
-    if (pri.beans.locateBean(beanname) != null ||
-        blankcontext.containsBean(beanname) ) {
+    if (pri.beans.locateBean(beanname) != null
+        || blankcontext.containsBean(beanname)) {
       bean = getLocalBean(pri, beanname, nolazy);
     }
     else {
-      if (bean == null) {
+      if (bean == null && this.parentcontext.containsBean(beanname)) {
         bean = this.parentcontext.getBean(beanname);
       }
     }
@@ -344,149 +347,164 @@ public class RSACBeanLocator implements ApplicationContextAware,
 
   private Object createBean(final PerRequestInfo pri, final String beanname) {
     CreationMarker marker = (CreationMarker) pri.beans.locateBean(beanname);
+    boolean success = false;
     if (marker == null) {
       marker = BEAN_IN_CREATION_OBJECT;
       pri.beans.set(beanname, marker);
     }
-    RSACBeanInfo rbi = (RSACBeanInfo) rbimap.get(beanname);
-    if (rbi == null) {
-      throw new NoSuchBeanDefinitionException(beanname,
-          "Bean definition not found");
-    }
-    // implement fetch wrappers in such a way that doesn't slow normal creation.
-    if (rbi.fetchwrappers != null
-        && marker.wrapperindex < rbi.fetchwrappers.length) {
-      if (marker.wrapperindex == 0) {
-        pri.beans.set(beanname, new CreationMarker(1));
+    try {
+      RSACBeanInfo rbi = (RSACBeanInfo) rbimap.get(beanname);
+      if (rbi == null) {
+        throw new NoSuchBeanDefinitionException(beanname,
+            "Bean definition not found");
+      }
+      // implement fetch wrappers in such a way that doesn't slow normal
+      // creation.
+      if (rbi.fetchwrappers != null
+          && marker.wrapperindex < rbi.fetchwrappers.length) {
+        if (marker.wrapperindex == 0) {
+          pri.beans.set(beanname, new CreationMarker(1));
+        }
+        else {
+          ++marker.wrapperindex;
+        }
+        Object wrappero = rbi.fetchwrappers[marker.wrapperindex];
+        RunnableWrapper wrapper = (RunnableWrapper) (wrappero instanceof RunnableWrapper ? wrappero
+            : getBean(pri, (String) wrappero, true));
+        final Object[] togo = new Object[1];
+        wrapper.wrapRunnable(new Runnable() {
+          public void run() {
+            togo[0] = createBean(pri, beanname);
+          }
+
+        }).run();
+        return togo[0];
+      }
+      ++pri.cbeans;
+
+      Object newbean;
+      // NB - isn't this odd, and in fact generally undocumented - properties
+      // defined for factory-method beans are set on the PRODUCT, whereas those
+      // set on FactoryBeans are set on the FACTORY!!
+      if (rbi.factorybean != null) {
+        Object factorybean = getBean(pri, rbi.factorybean, false);
+        newbean = reflectivecache.invokeMethod(factorybean, rbi.factorymethod);
+        if (newbean == null) {
+          throw new IllegalArgumentException(
+              "Error: null returned from factory method " + rbi.factorymethod
+                  + " of bean " + rbi.factorybean);
+        }
+        // rbi.beanclass = newbean.getClass();
       }
       else {
-        ++marker.wrapperindex;
+        // Locate the "dead" bean from the genuine Spring context, and clone it
+        // as quick as we can - bytecodes might do faster but in the meantime
+        // observe that a clone typically costs 1.6 reflective calls so in
+        // general
+        // this method will win over a reflective solution.
+        // NB - all Copiables simply copy dependencies manually for now, no
+        // cost.
+        // Copiable deadbean = (Copiable) livecontext.getBean(rbi.isfactorybean?
+        // "&" +beanname : beanname);
+        // All the same, the following line will cost us close to 1us - unless
+        // it
+        // invokes manual code!
+        newbean = reflectivecache.construct(rbi.beanclass);
       }
-      Object wrappero = rbi.fetchwrappers[marker.wrapperindex];
-      RunnableWrapper wrapper = (RunnableWrapper) (wrappero instanceof RunnableWrapper ? wrappero
-          : getBean(pri, (String) wrappero, true));
-      final Object[] togo = new Object[1];
-      wrapper.wrapRunnable(new Runnable() {
-        public void run() {
-          togo[0] = createBean(pri, beanname);
-        }
-
-      }).run();
-      return togo[0];
-    }
-    ++pri.cbeans;
-
-    Object newbean;
-    // NB - isn't this odd, and in fact generally undocumented - properties
-    // defined for factory-method beans are set on the PRODUCT, whereas those
-    // set on FactoryBeans are set on the FACTORY!!
-    if (rbi.factorybean != null) {
-      Object factorybean = getBean(pri, rbi.factorybean, false);
-      newbean = reflectivecache.invokeMethod(factorybean, rbi.factorymethod);
-      if (newbean == null) {
-        throw new IllegalArgumentException(
-            "Error: null returned from factory method " + rbi.factorymethod
-                + " of bean " + rbi.factorybean);
-      }
-      // rbi.beanclass = newbean.getClass();
-    }
-    else {
-      // Locate the "dead" bean from the genuine Spring context, and clone it
-      // as quick as we can - bytecodes might do faster but in the meantime
-      // observe that a clone typically costs 1.6 reflective calls so in general
-      // this method will win over a reflective solution.
-      // NB - all Copiables simply copy dependencies manually for now, no cost.
-      // Copiable deadbean = (Copiable) livecontext.getBean(rbi.isfactorybean?
-      // "&" +beanname : beanname);
-      // All the same, the following line will cost us close to 1us - unless it
-      // invokes manual code!
-      newbean = reflectivecache.construct(rbi.beanclass);
-    }
-    if (rbi.hasDependencies()) {
-      // guard this block since if it is a factory-method bean it may be
-      // something
-      // extremely undesirable (like an inner class) that we should not even
-      // dream of reflecting over. If on the other hand the user has specified
-      // some dependencies they doubtless know what they are doing.
-      MethodAnalyser ma = smc.getAnalyser(newbean.getClass());
-      // Object clonebean = deadbean.copy();
-      // iterate over each LOCAL dependency of the bean with given name.
-      for (Iterator depit = rbi.dependencies(); depit.hasNext();) {
-        String propertyname = (String) depit.next();
-        try {
-          AccessMethod setter = ma.getAccessMethod(propertyname);
-          Object depbean = null;
-          Object beanref = rbi.beannames(propertyname);
-          if (beanref instanceof String) {
-            depbean = getBean(pri, (String) beanref, false);
-          }
-          else if (beanref instanceof ValueHolder) {
-            Class accezzz = setter.getAccessedType();
-            String value = ((ValueHolder) beanref).value;
-            if (smc.saxleafparser.isLeafType(accezzz)) {
-              depbean = smc.saxleafparser.parse(accezzz, value);
+      if (rbi.hasDependencies()) {
+        // guard this block since if it is a factory-method bean it may be
+        // something
+        // extremely undesirable (like an inner class) that we should not even
+        // dream of reflecting over. If on the other hand the user has specified
+        // some dependencies they doubtless know what they are doing.
+        MethodAnalyser ma = smc.getAnalyser(newbean.getClass());
+        // Object clonebean = deadbean.copy();
+        // iterate over each LOCAL dependency of the bean with given name.
+        for (Iterator depit = rbi.dependencies(); depit.hasNext();) {
+          String propertyname = (String) depit.next();
+          try {
+            AccessMethod setter = ma.getAccessMethod(propertyname);
+            Object depbean = null;
+            Object beanref = rbi.beannames(propertyname);
+            if (beanref instanceof String) {
+              depbean = getBean(pri, (String) beanref, false);
+            }
+            else if (beanref instanceof ValueHolder) {
+              Class accezzz = setter.getAccessedType();
+              String value = ((ValueHolder) beanref).value;
+              if (smc.saxleafparser.isLeafType(accezzz)) {
+                depbean = smc.saxleafparser.parse(accezzz, value);
+              }
+              else {
+                // exception def copied from the beast BeanWrapperImpl!
+                throw new TypeMismatchException(new PropertyChangeEvent(
+                    newbean, propertyname, null, value), accezzz, null);
+              }
             }
             else {
-              // exception def copied from the beast BeanWrapperImpl!
-              throw new TypeMismatchException(new PropertyChangeEvent(newbean,
-                  propertyname, null, value), accezzz, null);
+              // Really need generalised conversion of vector values here.
+              // The code to do this is actually WITHIN the grotty
+              // BeanWrapperImpl
+              // itself in a protected method with 5 arguments!!
+              // This is a sort of 50% solution. It will deal with all 1-d array
+              // types and collections, and values of parseable types.
+              depbean = assembleVectorProperty(pri, (StringList) beanref,
+                  setter.getDeclaredType());
             }
+            // Lose another 500ns here, until we bring on FastClass.
+            setter.setChildObject(newbean, depbean);
           }
-          else {
-            // Really need generalised conversion of vector values here.
-            // The code to do this is actually WITHIN the grotty BeanWrapperImpl
-            // itself in a protected method with 5 arguments!!
-            // This is a sort of 50% solution. It will deal with all 1-d array
-            // types and collections, and values of parseable types.
-            depbean = assembleVectorProperty(pri, (StringList) beanref, setter
-                .getDeclaredType());
+          catch (Exception e) {
+            throw UniversalRuntimeException.accumulate(e,
+                "Error setting dependency " + propertyname + " of bean "
+                    + beanname);
           }
-          // Lose another 500ns here, until we bring on FastClass.
-          setter.setChildObject(newbean, depbean);
+        }
+      }
+      if (rbi.dependson != null) {
+        for (int i = 0; i < rbi.dependson.length; ++i) {
+          getBean(pri, rbi.dependson[i], false);
+        }
+      }
+      // process it FIRST since it will be the factory that is expecting the
+      // dependencies set!
+      processNewBean(pri, beanname, newbean);
+      // now the bean is initialised, attempt to call any init-method or
+      // InitBean.
+      if (rbi.initmethod != null) {
+        reflectivecache.invokeMethod(newbean, rbi.initmethod);
+      }
+      if (newbean instanceof InitializingBean) {
+        try {
+          ((InitializingBean) newbean).afterPropertiesSet();
+        }
+        catch (Exception e) { // Evil Rod! Bad Juergen!
+          throw UniversalRuntimeException.accumulate(e);
+        }
+      }
+      if (rbi.destroymethod != null) {
+        pri.todestroy.add(beanname);
+      }
+      if (newbean instanceof FactoryBean) {
+        FactoryBean factorybean = (FactoryBean) newbean;
+        try {
+          newbean = factorybean.getObject();
         }
         catch (Exception e) {
-          throw UniversalRuntimeException.accumulate(e,
-              "Error setting dependency " + propertyname + " of bean "
-                  + beanname);
+          throw UniversalRuntimeException.accumulate(e);
         }
       }
+      // enter the bean into the req-specific map.
+      pri.beans.set(beanname, newbean);
+      success = true;
+      return newbean;
     }
-    if (rbi.dependson != null) {
-      for (int i = 0; i < rbi.dependson.length; ++i) {
-        getBean(pri, rbi.dependson[i], false);
+    finally {
+      if (marker == BEAN_IN_CREATION_OBJECT && !success) {
+        pri.beans.remove(beanname);
       }
     }
-    // process it FIRST since it will be the factory that is expecting the
-    // dependencies set!
-    processNewBean(pri, beanname, newbean);
-    // now the bean is initialised, attempt to call any init-method or InitBean.
-    if (rbi.initmethod != null) {
-      reflectivecache.invokeMethod(newbean, rbi.initmethod);
-    }
-    if (newbean instanceof InitializingBean) {
-      try {
-        ((InitializingBean) newbean).afterPropertiesSet();
-      }
-      catch (Exception e) { // Evil Rod! Bad Juergen!
-        throw UniversalRuntimeException.accumulate(e);
-      }
-    }
-    if (rbi.destroymethod != null) {
-      pri.todestroy.add(beanname);
-    }
-    if (newbean instanceof FactoryBean) {
-      FactoryBean factorybean = (FactoryBean) newbean;
-      try {
-        newbean = factorybean.getObject();
-      }
-      catch (Exception e) {
-        throw UniversalRuntimeException.accumulate(e);
-      }
-    }
-    // enter the bean into the req-specific map.
-    pri.beans.set(beanname, newbean);
 
-    return newbean;
   }
 
   private Object processNewBean(PerRequestInfo pri, String beanname,
@@ -496,8 +514,8 @@ public class RSACBeanLocator implements ApplicationContextAware,
       try {
         newbean = beanpp.postProcessBeforeInitialization(newbean, beanname);
         // TODO: Timing of this next line is incorrect - should occur after
-        // initialisation in caller. 
-        //beanpp.postProcessAfterInitialization(newbean, beanname);
+        // initialisation in caller.
+        // beanpp.postProcessAfterInitialization(newbean, beanname);
       }
       catch (Exception e) {
         Logger.log.log(Level.ERROR, "Exception processing bean "
