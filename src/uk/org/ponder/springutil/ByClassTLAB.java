14498b3248d681ab2a6f8d1078a4a13b5ab54b39
Antranig Basman <amb26@ponder.org.uk>
2006-09-19 19:59:59 +0000

    StaticTemplateResolverStrategy introduced Old apps working once more

--- src/uk/org/ponder/springutil/ByClassTLAB.java
+++ src/uk/org/ponder/springutil/ByClassTLAB.java
@@ -3,30 +3,53 @@
  */
 package uk.org.ponder.springutil;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 import uk.org.ponder.stringutil.StringList;
 
-/** A TargetListAggregatingBean that will acquire all beans of a particular
- * type (excluding Factory products) and automatically make them 
- *  request-addressible.
+/**
+ * A TargetListAggregatingBean that will acquire all beans of a particular type
+ * (excluding Factory products) and register their *names* as the TLAB target.
+ * 
  * @author Antranig Basman (antranig@caret.cam.ac.uk)
- *
+ * 
  */
 
 public class ByClassTLAB extends StaticTLAB implements ApplicationContextAware {
   private Class targetClass;
+  private boolean deliverBeans;
 
   public void setTargetClass(Class targetClass) {
     this.targetClass = targetClass;
   }
-  
-  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-    String[] beannames = applicationContext.getBeanNamesForType(
-        targetClass, false, false);
-    setValue(new StringList(beannames));
+
+  /**
+   * If set to <code>true</code> (the default is <code>false</code> this
+   * will deliver a list the actual beans as the TLAB value rather than just a
+   * list of their names.
+   */
+  public void setDeliverBeans(boolean deliverBeans) {
+    this.deliverBeans = deliverBeans;
+  }
+
+  public void setApplicationContext(ApplicationContext applicationContext)
+      throws BeansException {
+    String[] beannames = applicationContext.getBeanNamesForType(targetClass,
+        false, false);
+    if (deliverBeans) {
+      ArrayList beans = new ArrayList();
+      for (int i = 0; i < beannames.length; ++i) {
+        beans.add(applicationContext.getBean(beannames[i]));
+      }
+      setValue(beans);
+    }
+    else {
+      setValue(new StringList(beannames));
+    }
   }
 
 }
