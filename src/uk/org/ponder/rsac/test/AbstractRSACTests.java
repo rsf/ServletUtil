6aece24c09cd5851ca6217f2ed6d400a70237b1e
Antranig Basman <amb26@ponder.org.uk>
2007-06-22 19:14:54 +0000

    PlainRSFTests created for "standard RSF context" AbstractRSACTests upgraded to be request-aware in setUp/tearDown Fix for single-value against array submission issue (RSF-28) as well as test fixture!

--- src/uk/org/ponder/rsac/test/AbstractRSACTests.java
+++ src/uk/org/ponder/rsac/test/AbstractRSACTests.java
@@ -8,21 +8,41 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
-public abstract class AbstractRSACTests extends AbstractDependencyInjectionSpringContextTests {
+import uk.org.ponder.rsac.RSACBeanLocator;
+
+public abstract class AbstractRSACTests extends
+    AbstractDependencyInjectionSpringContextTests {
+  private RSACBeanLocator rsacbl;
   public abstract String[] getRequestConfigLocations();
-  
-  protected ConfigurableApplicationContext loadContextLocations(String[] locations) {
+
+  protected ConfigurableApplicationContext loadContextLocations(
+      String[] locations) {
     final LocalRSACResourceLocator resourceLocator = new LocalRSACResourceLocator();
     resourceLocator.setConfigLocations(getRequestConfigLocations());
-    
-    final ConfigurableApplicationContext cac = new ClassPathXmlApplicationContext(locations, false) {
-      protected void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)  {
+
+    final ConfigurableApplicationContext cac = new ClassPathXmlApplicationContext(
+        locations, false) {
+      protected void postProcessBeanFactory(
+          ConfigurableListableBeanFactory beanFactory) {
         resourceLocator.setApplicationContext(this);
         beanFactory.registerSingleton("RSACResourceLocator", resourceLocator);
       }
     };
-  
+
     cac.refresh();
     return cac;
+  }
+
+  public RSACBeanLocator getRSACBeanLocator() {
+    return rsacbl;
+  }
+  
+  protected void onSetUp() throws Exception {
+    rsacbl = (RSACBeanLocator) applicationContext.getBean("RSACBeanLocator");
+    rsacbl.startRequest();
+  }
+
+  protected void onTearDown() throws Exception {
+    rsacbl.endRequest();
   }
 }
