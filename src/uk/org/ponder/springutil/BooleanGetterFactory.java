d6dc0701a4fe871f58cc22e5b0e837ab2220012d
Antranig Basman <amb26@ponder.org.uk>
2007-09-04 23:57:59 +0000

    Correction for impl and Java 1.4-ity of BGF

--- src/uk/org/ponder/springutil/BooleanGetterFactory.java
+++ src/uk/org/ponder/springutil/BooleanGetterFactory.java
@@ -3,10 +3,12 @@
  */
 package uk.org.ponder.springutil;
 
+import org.springframework.beans.factory.FactoryBean;
+
 import uk.org.ponder.booleanutil.BooleanGetter;
 import uk.org.ponder.booleanutil.BooleanHolder;
 
-public class BooleanGetterFactory {
+public class BooleanGetterFactory implements FactoryBean {
   
     private Boolean value;
 
@@ -15,7 +17,7 @@ public class BooleanGetterFactory {
     }
     
     public Object getObject() throws Exception {
-      return new BooleanHolder(value);
+      return new BooleanHolder(value.booleanValue());
     }
 
     public Class getObjectType() {
